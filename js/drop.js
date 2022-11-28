732aacbb754e3e159e7691036e4d36d9c5fd2d63
Ferry Ayunda <119156813+luhsiapa@users.noreply.github.com>
2022-11-28 11:59:25 +0700

    Update drop.js

--- js/drop.js
+++ js/drop.js
@@ -1,6 +1,5 @@
 // js
 
-document.getElementById('danger').textContent = "";
 $('[type="file"]').ezdz({
   
   text: 'drop a picture.',
@@ -17,14 +16,11 @@ $('[type="file"]').ezdz({
     }
       
     if (errors.maxWidth) {
-  //    alert(file.name + ' harus lebar:1024px max');
-        document.getElementById('danger').textContent = "file.name + ' harus lebar:1024px max' ";
-
+      alert(file.name + ' harus lebar:1024px max');        
     }
 
     if (errors.maxHeight) {
-    //  alert(file.name + ' harus tinggi:1024px max');
-        document.getElementById('danger').textContent = "file.name + ' harus tinggi:1024px max' ";
+      alert(file.name + ' harus tinggi:1024px max');        
     }
     
   }
