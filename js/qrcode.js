d071f52451356bd696bfa1a70024bb84e6651ff4
Ferry Ayunda <119156813+luhsiapa@users.noreply.github.com>
2022-11-29 22:17:50 +0700

    Update qrcode.js

--- js/qrcode.js
+++ js/qrcode.js
@@ -4,11 +4,12 @@ const genBtn = document.querySelector(".gen-btn");
 const qrCodeBox = document.querySelector(".qr-code");
 const input = document.querySelector("input");
 const image = document.querySelector(".qr");
+document.getElementById('warning').textContent = "";
 
 genBtn.addEventListener("click", () => {
   let inputValue = input.value;
   if (!inputValue) {
-    return alert("Harap masukkan teks atau URL!");
+    return document.getElementById('warning').textContent = "Harap masukkan teks atau URL!"; //alert("Harap masukkan teks atau URL!");
   }
   genBtn.innerHTML = 'Converting <i class="fa-solid fa-sync fa-spin"></i>';
   image.src = `https://chart.googleapis.com/chart?cht=qr&choe=UTF-8&chs=350x350&chl=${inputValue}`;
